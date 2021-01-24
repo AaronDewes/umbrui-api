fcdb5c28fcc17f37c29f3cda1980ba4dc5726806
Aaron Dewes <aaron.dewes@web.de>
2021-01-24 15:44:59 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = 3010;
+const fs = require('fs');
+
+
+app.use(bodyParser.json({ type: 'application/json' }));
+app.use(cors());
+
+app.get('/version', (req, res) => {
+  res.send('0');
+})
+
+app.post('/save', (req, res) => {
+  fs.writeFile('./data.json', JSON.stringify(req.body), (err) => {
+    if (err) {
+      res.send('false');
+    } else {
+      res.send('true');
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
+})
